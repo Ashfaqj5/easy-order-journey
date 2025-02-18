65dfdbe8f8e2cd8e8eced84f24e50d7fb9f94a3f
ashfaqj5 <786muhammedashfaq@gmail.com>
2025-02-18 22:46:14 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     mode === 'development' &&
-    componentTagger(),
+    // componentTagger(),
     VitePWA({
       registerType: 'autoUpdate',
       devOptions: {
